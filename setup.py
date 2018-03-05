71f91373b83a83687d877b3416aa646cc02e31c8
Petr Muller <afri@afri.cz>
2018-03-05 22:01:04 +0100

    Remove Python 3.3 from supported versions
    Mypy itself needs Python 3.4 at least, citing it's README.md:
    
    > You need Python 3.4 or later to run mypy

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         'Topic :: Software Development :: Testing',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
