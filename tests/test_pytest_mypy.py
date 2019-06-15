92cf9ba1db134e8adb29425f8884dbc38e72abae
David Tucker <david@tucker.name>
2019-06-15 00:28:54 -0700

    Add test_mypy_ignore_missings_imports

--- tests/test_pytest_mypy.py
+++ tests/test_pytest_mypy.py
@@ -23,3 +23,22 @@ def test_mypy_error(testdir):
         'test_mypy_error.py:2: error: Incompatible return value*',
     ])
     assert result.ret != 0
+
+
+def test_mypy_ignore_missings_imports(testdir):
+    """
+    Verify that --mypy-ignore-missing-imports
+    causes mypy to ignore missing imports.
+    """
+    testdir.makepyfile('''
+        import pytest_mypy
+    ''')
+    result = testdir.runpytest_subprocess('--mypy')
+    result.stdout.fnmatch_lines([
+        '*1: error: Cannot find module named*',
+        '* 1 failed *',
+    ])
+    assert result.ret != 0
+    result = testdir.runpytest_subprocess('--mypy-ignore-missing-imports')
+    result.stdout.fnmatch_lines(['* 1 passed *'])
+    assert result.ret == 0
