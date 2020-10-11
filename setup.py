1b7155bcf8741e288b5f0d9d7f0934f90c0b4a4e
David Tucker <david@tucker.name>
2020-10-11 10:21:27 -0700

    Add Black and flake8-commas

--- setup.py
+++ setup.py
@@ -9,58 +9,51 @@ from setuptools import setup, find_packages  # type: ignore
 
 def read(fname):
     file_path = os.path.join(os.path.dirname(__file__), fname)
-    return codecs.open(file_path, encoding='utf-8').read()
+    return codecs.open(file_path, encoding="utf-8").read()
 
 
 setup(
-    name='pytest-mypy',
+    name="pytest-mypy",
     use_scm_version=True,
-    author='Daniel Bader',
-    author_email='mail@dbader.org',
-    maintainer='David Tucker',
-    maintainer_email='david@tucker.name',
-    license='MIT',
-    url='https://github.com/dbader/pytest-mypy',
-    description='Mypy static type checker plugin for Pytest',
-    long_description=read('README.rst'),
-    long_description_content_type='text/x-rst',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    author="Daniel Bader",
+    author_email="mail@dbader.org",
+    maintainer="David Tucker",
+    maintainer_email="david@tucker.name",
+    license="MIT",
+    url="https://github.com/dbader/pytest-mypy",
+    description="Mypy static type checker plugin for Pytest",
+    long_description=read("README.rst"),
+    long_description_content_type="text/x-rst",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     py_modules=[
-        os.path.splitext(os.path.basename(path))[0]
-        for path in glob.glob('src/*.py')
+        os.path.splitext(os.path.basename(path))[0] for path in glob.glob("src/*.py")
     ],
-    python_requires='>=3.5',
-    setup_requires=[
-        'setuptools-scm>=3.5',
-    ],
+    python_requires=">=3.5",
+    setup_requires=["setuptools-scm>=3.5"],
     install_requires=[
-        'attrs>=19.0',
-        'filelock>=3.0',
-        'pytest>=3.5',
+        "attrs>=19.0",
+        "filelock>=3.0",
+        "pytest>=3.5",
         'mypy>=0.500; python_version<"3.8"',
         'mypy>=0.700; python_version>="3.8" and python_version<"3.9"',
         'mypy>=0.780; python_version>="3.9"',
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: Pytest',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Testing',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: MIT License',
+        "Development Status :: 4 - Beta",
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
     ],
-    entry_points={
-        'pytest11': [
-            'mypy = pytest_mypy',
-        ],
-    },
+    entry_points={"pytest11": ["mypy = pytest_mypy"]},
 )
--- tests/test_pytest_mypy.py
+++ tests/test_pytest_mypy.py
@@ -13,25 +13,27 @@ import pytest
 )
 def xdist_args(request):
     if request.param is None:
-        return ['-p', 'no:xdist']
-    return ['-n', 'auto'] if request.param else []
+        return ["-p", "no:xdist"]
+    return ["-n", "auto"] if request.param else []
 
 
-@pytest.mark.parametrize('pyfile_count', [1, 2])
+@pytest.mark.parametrize("pyfile_count", [1, 2])
 def test_mypy_success(testdir, pyfile_count, xdist_args):
     """Verify that running on a module with no type errors passes."""
     testdir.makepyfile(
         **{
-            'pyfile_' + str(pyfile_i): '''
+            "pyfile_{0}".format(
+                pyfile_i,
+            ): """
                 def pyfunc(x: int) -> int:
                     return x * 2
-            '''
+            """
             for pyfile_i in range(pyfile_count)
-        }
+        },
     )
     result = testdir.runpytest_subprocess(*xdist_args)
     result.assert_outcomes()
-    result = testdir.runpytest_subprocess('--mypy', *xdist_args)
+    result = testdir.runpytest_subprocess("--mypy", *xdist_args)
     mypy_file_checks = pyfile_count
     mypy_status_check = 1
     mypy_checks = mypy_file_checks + mypy_status_check
@@ -46,22 +48,23 @@ def test_mypy_pyi(testdir, xdist_args):
     """
     # The incorrect signature below should be ignored
     # as the .pyi file takes priority
-    testdir.makefile(
-        '.py', pyfile='''
+    testdir.makepyfile(
+        pyfile="""
             def pyfunc(x: int) -> str:
                 return x * 2
-        '''
+        """,
     )
 
     testdir.makefile(
-        '.pyi', pyfile='''
+        ".pyi",
+        pyfile="""
             def pyfunc(x: int) -> int: ...
-        '''
+        """,
     )
 
     result = testdir.runpytest_subprocess(*xdist_args)
     result.assert_outcomes()
-    result = testdir.runpytest_subprocess('--mypy', *xdist_args)
+    result = testdir.runpytest_subprocess("--mypy", *xdist_args)
     mypy_file_checks = 1
     mypy_status_check = 1
     mypy_checks = mypy_file_checks + mypy_status_check
@@ -71,20 +74,20 @@ def test_mypy_pyi(testdir, xdist_args):
 
 def test_mypy_error(testdir, xdist_args):
     """Verify that running on a module with type errors fails."""
-    testdir.makepyfile('''
-        def pyfunc(x: int) -> str:
-            return x * 2
-    ''')
+    testdir.makepyfile(
+        """
+            def pyfunc(x: int) -> str:
+                return x * 2
+        """,
+    )
     result = testdir.runpytest_subprocess(*xdist_args)
     result.assert_outcomes()
-    result = testdir.runpytest_subprocess('--mypy', *xdist_args)
+    result = testdir.runpytest_subprocess("--mypy", *xdist_args)
     mypy_file_checks = 1
     mypy_status_check = 1
     mypy_checks = mypy_file_checks + mypy_status_check
     result.assert_outcomes(failed=mypy_checks)
-    result.stdout.fnmatch_lines([
-        '2: error: Incompatible return value*',
-    ])
+    result.stdout.fnmatch_lines(["2: error: Incompatible return value*"])
     assert result.ret != 0
 
 
@@ -93,184 +96,216 @@ def test_mypy_ignore_missings_imports(testdir, xdist_args):
     Verify that --mypy-ignore-missing-imports
     causes mypy to ignore missing imports.
     """
-    module_name = 'is_always_missing'
-    testdir.makepyfile('''
-        try:
-            import {module_name}
-        except ImportError:
-            pass
-    '''.format(module_name=module_name))
-    result = testdir.runpytest_subprocess('--mypy', *xdist_args)
+    module_name = "is_always_missing"
+    testdir.makepyfile(
+        """
+            try:
+                import {module_name}
+            except ImportError:
+                pass
+        """.format(
+            module_name=module_name,
+        ),
+    )
+    result = testdir.runpytest_subprocess("--mypy", *xdist_args)
     mypy_file_checks = 1
     mypy_status_check = 1
     mypy_checks = mypy_file_checks + mypy_status_check
     result.assert_outcomes(failed=mypy_checks)
-    result.stdout.fnmatch_lines([
-        "2: error: Cannot find *module named '{module_name}'".format(
-            module_name=module_name,
-        ),
-    ])
-    assert result.ret != 0
-    result = testdir.runpytest_subprocess(
-        '--mypy-ignore-missing-imports',
-        *xdist_args
+    result.stdout.fnmatch_lines(
+        [
+            "2: error: Cannot find *module named '{module_name}'".format(
+                module_name=module_name,
+            ),
+        ],
     )
+    assert result.ret != 0
+    result = testdir.runpytest_subprocess("--mypy-ignore-missing-imports", *xdist_args)
     result.assert_outcomes(passed=mypy_checks)
     assert result.ret == 0
 
 
 def test_mypy_marker(testdir, xdist_args):
     """Verify that -m mypy only runs the mypy tests."""
-    testdir.makepyfile('''
-        def test_fails():
-            assert False
-    ''')
-    result = testdir.runpytest_subprocess('--mypy', *xdist_args)
+    testdir.makepyfile(
+        """
+            def test_fails():
+                assert False
+        """,
+    )
+    result = testdir.runpytest_subprocess("--mypy", *xdist_args)
     test_count = 1
     mypy_file_checks = 1
     mypy_status_check = 1
     mypy_checks = mypy_file_checks + mypy_status_check
     result.assert_outcomes(failed=test_count, passed=mypy_checks)
     assert result.ret != 0
-    result = testdir.runpytest_subprocess('--mypy', '-m', 'mypy', *xdist_args)
+    result = testdir.runpytest_subprocess("--mypy", "-m", "mypy", *xdist_args)
     result.assert_outcomes(passed=mypy_checks)
     assert result.ret == 0
 
 
 def test_non_mypy_error(testdir, xdist_args):
     """Verify that non-MypyError exceptions are passed through the plugin."""
-    message = 'This is not a MypyError.'
-    testdir.makepyfile(conftest='''
-        def pytest_configure(config):
-            plugin = config.pluginmanager.getplugin('mypy')
+    message = "This is not a MypyError."
+    testdir.makepyfile(
+        conftest="""
+            def pytest_configure(config):
+                plugin = config.pluginmanager.getplugin('mypy')
 
-            class PatchedMypyFileItem(plugin.MypyFileItem):
-                def runtest(self):
-                    raise Exception('{message}')
+                class PatchedMypyFileItem(plugin.MypyFileItem):
+                    def runtest(self):
+                        raise Exception('{message}')
 
-            plugin.MypyFileItem = PatchedMypyFileItem
-    '''.format(message=message))
+                plugin.MypyFileItem = PatchedMypyFileItem
+        """.format(
+            message=message,
+        ),
+    )
     result = testdir.runpytest_subprocess(*xdist_args)
     result.assert_outcomes()
-    result = testdir.runpytest_subprocess('--mypy', *xdist_args)
+    result = testdir.runpytest_subprocess("--mypy", *xdist_args)
     mypy_file_checks = 1  # conftest.py
     mypy_status_check = 1
     result.assert_outcomes(
         failed=mypy_file_checks,  # patched to raise an Exception
         passed=mypy_status_check,  # conftest.py has no type errors.
     )
-    result.stdout.fnmatch_lines(['*' + message])
+    result.stdout.fnmatch_lines(["*" + message])
     assert result.ret != 0
 
 
 def test_mypy_stderr(testdir, xdist_args):
     """Verify that stderr from mypy is printed."""
-    stderr = 'This is stderr from mypy.'
-    testdir.makepyfile(conftest='''
-        import mypy.api
+    stderr = "This is stderr from mypy."
+    testdir.makepyfile(
+        conftest="""
+            import mypy.api
 
-        def _patched_run(*args, **kwargs):
-            return '', '{stderr}', 1
+            def _patched_run(*args, **kwargs):
+                return '', '{stderr}', 1
 
-        mypy.api.run = _patched_run
-    '''.format(stderr=stderr))
-    result = testdir.runpytest_subprocess('--mypy', *xdist_args)
+            mypy.api.run = _patched_run
+        """.format(
+            stderr=stderr,
+        ),
+    )
+    result = testdir.runpytest_subprocess("--mypy", *xdist_args)
     result.stdout.fnmatch_lines([stderr])
 
 
 def test_mypy_unmatched_stdout(testdir, xdist_args):
     """Verify that unexpected output on stdout from mypy is printed."""
-    stdout = 'This is unexpected output on stdout from mypy.'
-    testdir.makepyfile(conftest='''
-        import mypy.api
+    stdout = "This is unexpected output on stdout from mypy."
+    testdir.makepyfile(
+        conftest="""
+            import mypy.api
 
-        def _patched_run(*args, **kwargs):
-            return '{stdout}', '', 1
+            def _patched_run(*args, **kwargs):
+                return '{stdout}', '', 1
 
-        mypy.api.run = _patched_run
-    '''.format(stdout=stdout))
-    result = testdir.runpytest_subprocess('--mypy', *xdist_args)
+            mypy.api.run = _patched_run
+        """.format(
+            stdout=stdout,
+        ),
+    )
+    result = testdir.runpytest_subprocess("--mypy", *xdist_args)
     result.stdout.fnmatch_lines([stdout])
 
 
 def test_api_mypy_argv(testdir, xdist_args):
     """Ensure that the plugin can be configured in a conftest.py."""
-    testdir.makepyfile(conftest='''
-        def pytest_configure(config):
-            plugin = config.pluginmanager.getplugin('mypy')
-            plugin.mypy_argv.append('--version')
-    ''')
-    result = testdir.runpytest_subprocess('--mypy', *xdist_args)
+    testdir.makepyfile(
+        conftest="""
+            def pytest_configure(config):
+                plugin = config.pluginmanager.getplugin('mypy')
+                plugin.mypy_argv.append('--version')
+        """,
+    )
+    result = testdir.runpytest_subprocess("--mypy", *xdist_args)
     assert result.ret == 0
 
 
 def test_api_nodeid_name(testdir, xdist_args):
     """Ensure that the plugin can be configured in a conftest.py."""
-    nodeid_name = 'UnmistakableNodeIDName'
-    testdir.makepyfile(conftest='''
-        def pytest_configure(config):
-            plugin = config.pluginmanager.getplugin('mypy')
-            plugin.nodeid_name = '{}'
-    '''.format(nodeid_name))
-    result = testdir.runpytest_subprocess('--mypy', '--verbose', *xdist_args)
-    result.stdout.fnmatch_lines(['*conftest.py::' + nodeid_name + '*'])
+    nodeid_name = "UnmistakableNodeIDName"
+    testdir.makepyfile(
+        conftest="""
+            def pytest_configure(config):
+                plugin = config.pluginmanager.getplugin('mypy')
+                plugin.nodeid_name = '{}'
+        """.format(
+            nodeid_name,
+        ),
+    )
+    result = testdir.runpytest_subprocess("--mypy", "--verbose", *xdist_args)
+    result.stdout.fnmatch_lines(["*conftest.py::" + nodeid_name + "*"])
     assert result.ret == 0
 
 
 def test_mypy_indirect(testdir, xdist_args):
     """Verify that uncollected files checked by mypy cause a failure."""
-    testdir.makepyfile(bad='''
-        def pyfunc(x: int) -> str:
-            return x * 2
-    ''')
-    testdir.makepyfile(good='''
-        import bad
-    ''')
-    result = testdir.runpytest_subprocess('--mypy', *xdist_args, 'good.py')
+    testdir.makepyfile(
+        bad="""
+            def pyfunc(x: int) -> str:
+                return x * 2
+        """,
+    )
+    testdir.makepyfile(
+        good="""
+            import bad
+        """,
+    )
+    result = testdir.runpytest_subprocess("--mypy", *xdist_args, "good.py")
     assert result.ret != 0
 
 
 def test_api_error_formatter(testdir, xdist_args):
     """Ensure that the plugin can be configured in a conftest.py."""
-    testdir.makepyfile(bad='''
-        def pyfunc(x: int) -> str:
-            return x * 2
-    ''')
-    testdir.makepyfile(conftest='''
-        def custom_file_error_formatter(item, results, errors):
-            return '\\n'.join(
-                '{path}:{error}'.format(
-                    path=item.fspath,
-                    error=error,
+    testdir.makepyfile(
+        bad="""
+            def pyfunc(x: int) -> str:
+                return x * 2
+        """,
+    )
+    testdir.makepyfile(
+        conftest="""
+            def custom_file_error_formatter(item, results, errors):
+                return '\\n'.join(
+                    '{path}:{error}'.format(
+                        path=item.fspath,
+                        error=error,
+                    )
+                    for error in errors
                 )
-                for error in errors
-            )
 
-        def pytest_configure(config):
-            plugin = config.pluginmanager.getplugin('mypy')
-            plugin.file_error_formatter = custom_file_error_formatter
-    ''')
-    result = testdir.runpytest_subprocess('--mypy', *xdist_args)
-    result.stdout.fnmatch_lines([
-        '*/bad.py:2: error: Incompatible return value*',
-    ])
+            def pytest_configure(config):
+                plugin = config.pluginmanager.getplugin('mypy')
+                plugin.file_error_formatter = custom_file_error_formatter
+        """,
+    )
+    result = testdir.runpytest_subprocess("--mypy", *xdist_args)
+    result.stdout.fnmatch_lines(["*/bad.py:2: error: Incompatible return value*"])
     assert result.ret != 0
 
 
 def test_setup_cfg(testdir, xdist_args):
     """Ensure that the plugin allows configuration with setup.cfg."""
-    testdir.makefile('.cfg', setup='''
-        [mypy]
-        disallow_untyped_defs = True
-    ''')
-    testdir.makepyfile(conftest='''
-        def pyfunc(x):
-            return x * 2
-    ''')
-    result = testdir.runpytest_subprocess('--mypy', *xdist_args)
-    result.stdout.fnmatch_lines([
-        '1: error: Function is missing a type annotation',
-    ])
+    testdir.makefile(
+        ".cfg",
+        setup="""
+            [mypy]
+            disallow_untyped_defs = True
+        """,
+    )
+    testdir.makepyfile(
+        conftest="""
+            def pyfunc(x):
+                return x * 2
+        """,
+    )
+    result = testdir.runpytest_subprocess("--mypy", *xdist_args)
+    result.stdout.fnmatch_lines(["1: error: Function is missing a type annotation"])
     assert result.ret != 0
 
 
